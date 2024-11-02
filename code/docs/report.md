d8139cb413a309f5acd98012a8c8e3cf529e11a6
mcenk <mcenkyediparmak@gmail.com>
2024-11-03 02:39:54 +0300

    report added to the docs

---
+++ code/docs/report.md
@@ -0,0 +1,5 @@
+# CENG 435 Programming Assignment Phase 1 Report
+
+Mert Cenk Yediparmak 2522167
+
+https://github.com/mcenkyediparmak/covertovert
--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,4 +1,4 @@
 import scapy.all
 
-#pkt = scapy.all.sniff(count=1, filter="icmp")[0]
+pkt = scapy.all.sniff(count=1, filter="icmp")[0]
 # Implement your ICMP receiver here
